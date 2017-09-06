54546f986948dc36e6d35b68d03359086dd048f7
aimerib <aimeri@baddouh.me>
2017-09-05 22:17:35 -0400

    100 Days of JS Challenge: Day 1

---
+++ playground/100dayschallenge/day 1/script.js
@@ -0,0 +1,29 @@
+var i = 0;
+var screams = ["You really clicked me!",
+               "Stop that!",
+               "It was just a joke...",
+               "What am I going to tell my wife?",
+               "This is my life now... Being poked insesantly...",
+               "Sigh... Are you not tired of the abuse yet?",
+               "Please stop!",
+               "AAAAAAH!",
+               "Alright pal! No more mister nice guy!",
+               "I dare you to click me one more time!!!!"
+             ]
+
+document.getElementById('myButton').addEventListener("click", function() {
+  document.body.style.backgroundColor = "black";
+  this.style.backgroundColor = "red";
+  this.style.color = "white";
+  this.style.fontFamily = "Frijole"
+  if(i <= 9 ){
+    this.innerHTML = screams[i];
+
+  }
+  else {
+    i = 0;
+    this.innerHTML = screams[i];
+  }
+
+  i++;
+});
