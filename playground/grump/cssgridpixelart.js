5594df059b0068384a5cee90ce54a6b05b832f3c
aimerib <aimeri@baddouh.me>
2017-10-11 01:16:05 -0400

    .

--- playground/grump/cssgridpixelart.js
+++ playground/grump/cssgridpixelart.js
@@ -1,36 +1,36 @@
 function cssGridPixelArt(url){
-  loadIconsFromServer(url, drawCanvas);
+    loadIconsFromServer(url, drawCanvas);
 }
 
 
 function loadIconsFromServer(url, callDrawCanvas){
 
-  var getPixelArtObjectRequest = new XMLHttpRequest();
+    const getPixelArtObjectRequest = new XMLHttpRequest();
 
-  getPixelArtObjectRequest.open("GET", url, true);
-  getPixelArtObjectRequest.send();
-  getPixelArtObjectRequest.onreadystatechange = function(){
+    getPixelArtObjectRequest.open('GET', url, true);
+    getPixelArtObjectRequest.send();
+    getPixelArtObjectRequest.onreadystatechange = function(){
     
-    if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4 && this.status == 200) {
       
-      callDrawCanvas(getPixelArtObjectRequest.responseText);
+            callDrawCanvas(getPixelArtObjectRequest.responseText);
 
-    }
+        }
 
-  };
+    };
 }
 
 
 
 function drawCanvas(loadIconsFromServerResponse) {
 
-  imgPixel = JSON.parse(loadIconsFromServerResponse);
-  var i = imgPixel.length;
+    let imgPixel = JSON.parse(loadIconsFromServerResponse);
+    let i = imgPixel.length;
 
-  while (i--){
-    styleCanvas(imgPixel[i].name, imgPixel[i].iconHeight, imgPixel[i].iconWidth);
-    drawIcon(imgPixel[i].name, imgPixel[i].iconHeight, imgPixel[i].iconWidth, imgPixel[i].pixelMap);
-  }
+    while (i--){
+        styleCanvas(imgPixel[i].name, imgPixel[i].iconHeight, imgPixel[i].iconWidth);
+        drawIcon(imgPixel[i].name, imgPixel[i].iconHeight, imgPixel[i].iconWidth, imgPixel[i].pixelMap);
+    }
 
 }
 
@@ -38,17 +38,17 @@ function drawCanvas(loadIconsFromServerResponse) {
 
 
 function styleCanvas(className, canvasHeight, canvasWidth) {
-  var canvasStyleSizePosition = "height:20vh; width:20vh; margin:0 auto;"
-  var canvasStyleDisplay = "display:grid;" 
-  var canvasStyleGridColumns = "grid-template-columns:repeat(" + canvasHeight + ", 1fr);"
-  var canvasStyleGridRows = "grid-template-rows:repeat(" + canvasWidth + ", 1fr)"
-  var canvasStyle = canvasStyleSizePosition + canvasStyleDisplay + canvasStyleGridColumns + canvasStyleGridRows
-  var canvasObjects = document.getElementsByClassName(className);
+    const canvasStyleSizePosition = 'height:20vh; width:20vh; margin:0 auto;';
+    const canvasStyleDisplay = 'display:grid;'; 
+    const canvasStyleGridColumns = 'grid-template-columns:repeat(' + canvasHeight + ', 1fr);';
+    const canvasStyleGridRows = 'grid-template-rows:repeat(' + canvasWidth + ', 1fr)';
+    const canvasStyle = canvasStyleSizePosition + canvasStyleDisplay + canvasStyleGridColumns + canvasStyleGridRows;
+    const canvasObjects = document.getElementsByClassName(className);
 
 
-  for (i = 0; i < canvasObjects.length; i++){
-    canvasObjects[i].setAttribute("style", canvasStyle);
-  }
+    for (let i = 0; i < canvasObjects.length; i++){
+        canvasObjects[i].setAttribute('style', canvasStyle);
+    }
   
 }
 
@@ -57,43 +57,43 @@ function styleCanvas(className, canvasHeight, canvasWidth) {
 
 function drawIcon(className, row, column, pixelOn) {
 
-  var gridDivStyle = [];
-  var iconNameCount = document.getElementsByClassName(className);
+    const gridDivStyle = [];
+    const iconNameCount = document.getElementsByClassName(className);
     
-  for (i = 0; i < row; i++) {
+    for (let i = 0; i < row; i++) {
     
-    gridDivStyle.push([]); //initializes array rows
+        gridDivStyle.push([]); //initializes array rows
 
-    for (j = 0; j < column; j++) {
+        for (let j = 0; j < column; j++) {
 
-      gridDivStyle[i][j] = "col-" + i + "-row-" + j;
+            gridDivStyle[i][j] = 'col-' + i + '-row-' + j;
 
-      for (k=0; k < iconNameCount.length; k++){
-        var gridDiv = document.createElement("div");
-        var iconName = document.getElementsByClassName(className);
+            for (let k=0; k < iconNameCount.length; k++){
+                const gridDiv = document.createElement('div');
+                const iconName = document.getElementsByClassName(className);
 
-        gridDiv.className = gridDivStyle[i][j];
-        iconName[k].appendChild(gridDiv);
+                gridDiv.className = gridDivStyle[i][j];
+                iconName[k].appendChild(gridDiv);
 
-        if (pixelOn[i][j]) { 
+                if (pixelOn[i][j]) { 
 
-          var pixelOnClass = "col-" + i + "-row-" + j;
-          var pixelOnObject = document.getElementsByClassName(pixelOnClass);
-          var pixelOnStyle = "background-color:limegreen;"
+                    const pixelOnClass = 'col-' + i + '-row-' + j;
+                    const pixelOnObject = document.getElementsByClassName(pixelOnClass);
+                    const pixelOnStyle = 'background-color:limegreen;';
 
-          for (l = 0; l < pixelOnObject.length; l++){
-            pixelOnObject[l].setAttribute("style", pixelOnStyle);
-          }
+                    for (let l = 0; l < pixelOnObject.length; l++){
+                        pixelOnObject[l].setAttribute('style', pixelOnStyle);
+                    }
   
+                }
+
+            }
+
+      
+      
         }
 
-      }
 
-      
-      
     }
-
-
-  }
 
 }
--- playground/grump/grump.js
+++ playground/grump/grump.js
@@ -1,55 +1,54 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var buttons = document.getElementsByClassName('button');
-  var logo = document.getElementsByClassName('grumplogo')[0];
-  buttons[0].addEventListener("click", function () {
-    var mainWindow = document.getElementsByClassName('window')
-    var speechBubble = document.getElementsByClassName('speechBubble')[0]
-    mainWindow[0].style.display = 'none';
-    speechBubble.innerHTML = '<p>Whoa dude! Where did that window go???</p>'
-
-  });
-  logo.addEventListener("click", function () {
-    var mainWindow = document.getElementsByClassName('window')[0]
-    var speechBubble = document.getElementsByClassName('speechBubble')[0]
-    if (mainWindow.style.display === 'none'){
-      mainWindow.style.display = '';
-      speechBubble.innerHTML = "<p>There you go, it's back again :)</p>"
+document.addEventListener('DOMContentLoaded', function() {
+    var buttons = document.getElementsByClassName('button');
+    var logo = document.getElementsByClassName('grumplogo')[0];
+    buttons[0].addEventListener('click', function () {
+        var mainWindow = document.getElementsByClassName('window');
+        var speechBubble = document.getElementsByClassName('speechBubble')[0];
+        mainWindow[0].style.display = 'none';
+        speechBubble.innerHTML = '<p>Whoa dude! Where did that window go???</p>';
+    });
+    logo.addEventListener('click', function () {
+        var mainWindow = document.getElementsByClassName('window')[0];
+        var speechBubble = document.getElementsByClassName('speechBubble')[0];
+        if (mainWindow.style.display === 'none'){
+            mainWindow.style.display = '';
+            speechBubble.innerHTML = '<p>There you go, it\'s back again :)</p>';
       
-    }
+        }
 
-  });
-  buttons[1].addEventListener("click", function () {
-    var contentWindow = document.getElementsByClassName('window-main')[0];
-    var mainWindow = document.getElementsByClassName('window')[0];
-    var bannerSize = document.getElementsByClassName('banner')[0].style.height
-    var bottomOfWindow = window.innerHeight
-    if (contentWindow.style.display === 'none'){contentWindow.style.display = ''}
-    else {contentWindow.style.display = 'none'}
-  });
-  buttons[2].addEventListener("click", function () {
-    var contentWindow = document.getElementsByClassName('window-main')[0];
-    var mainWindow = document.getElementsByClassName('window')[0];
-    var banner = document.getElementsByClassName('banner')[0];
-    var wrapper = document.getElementsByClassName('wrapper')[0];
-    if (contentWindow.style.display === 'none'){contentWindow.style.display = ''}
-    if (banner.style.display !== 'none'){
-      banner.style.display = 'none';
-      mainWindow.style.width = window.innerWidth;
-      mainWindow.style.position = 'absolute';
-      mainWindow.style.top = '0';
-      wrapper.style.height = window.innerHeight;
-      mainWindow.style.height = window.innerHeight;
-      contentWindow.style.height = window.innerHeight;
-    }
-    else {
-      banner.style.display = '';
-      mainWindow.style.width = '';
-      mainWindow.style.position = '';
-      mainWindow.style.top = '';
-      wrapper.style.height = '';
-      mainWindow.style.height = '';
-      contentWindow.style.height = '';
-    }
+    });
+    buttons[1].addEventListener('click', function () {
+        var contentWindow = document.getElementsByClassName('window-main')[0];
+        var mainWindow = document.getElementsByClassName('window')[0];
+        var bannerSize = document.getElementsByClassName('banner')[0].style.height;
+        var bottomOfWindow = window.innerHeight;
+        if (contentWindow.style.display === 'none'){contentWindow.style.display = ''}
+        else {contentWindow.style.display = 'none'}
+    });
+    buttons[2].addEventListener('click', function () {
+        var contentWindow = document.getElementsByClassName('window-main')[0];
+        var mainWindow = document.getElementsByClassName('window')[0];
+        var banner = document.getElementsByClassName('banner')[0];
+        var wrapper = document.getElementsByClassName('wrapper')[0];
+        if (contentWindow.style.display === 'none'){contentWindow.style.display = ''}
+        if (banner.style.display !== 'none'){
+            banner.style.display = 'none';
+            mainWindow.style.width = window.innerWidth;
+            mainWindow.style.position = 'absolute';
+            mainWindow.style.top = '0';
+            wrapper.style.height = window.innerHeight;
+            mainWindow.style.height = window.innerHeight;
+            contentWindow.style.height = window.innerHeight;
+        }
+        else {
+            banner.style.display = '';
+            mainWindow.style.width = '';
+            mainWindow.style.position = '';
+            mainWindow.style.top = '';
+            wrapper.style.height = '';
+            mainWindow.style.height = '';
+            contentWindow.style.height = '';
+        }
 
-  });
+    });
 });
